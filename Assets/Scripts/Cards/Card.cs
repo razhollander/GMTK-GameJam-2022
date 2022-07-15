8300518305bf7739882661b294c3ca8172d3620a
raz hollander <raz.hollander@monkeyleague.io>
2022-07-15 23:12:02 +0300

    temp art

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : ScriptableObject
+{
+    public Image CardImage;
+    public GameEvent CardGameEvent;
+}
