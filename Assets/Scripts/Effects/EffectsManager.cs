0d9227254abbffd5e9e9c8b83fc0579b0423a2cf
raz hollander <raz.hollander@monkeyleague.io>
2022-07-16 12:11:20 +0300

    alarm effect

--- Assets/Scripts/Effects/EffectsManager.cs
+++ Assets/Scripts/Effects/EffectsManager.cs
@@ -1,11 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class EffectsManager : GaneEventListener
 {
+    [SerializeField] private ParticleSystem _snowEffect;
+    [SerializeField] private VolumeProfile VolumeProfile;
+    [SerializeField] private Vector2 _alarmAnimationBounds = new Vector2(0.224f, 0.336f);
+    [SerializeField] private float _alarmAnimationTime = 2;
+
+    private Tween alarmAnimationTween;
+    
     public override void OnGameEvent(GameEvent gameEvent)
     {
+        StopAll();
         
+        switch (gameEvent)
+        {
+            case GameEvent.Freeze: 
+                _snowEffect.gameObject.SetActive(true);
+                _snowEffect.Play(); break;
+            case GameEvent.Alarm: 
+                 
+                Vignette vignette;
+ 
+                if(!VolumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
+                vignette.intensity.Override(_alarmAnimationBounds.x);
+                alarmAnimationTween = DOTween.To(() => vignette.intensity.value, value => vignette.intensity.Override(value), _alarmAnimationBounds.y, _alarmAnimationTime)
+                    .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InQuart); break;
+                default: break;
+        }
     }
+    
+    [ContextMenu("fire alarm effect")]
+    public void FireAlarm()
+    {
+        OnGameEvent(GameEvent.Alarm);
+    }
+    private void StopAll()
+    {
+        _snowEffect.gameObject.SetActive(false);
+        _snowEffect.Stop();
+        alarmAnimationTween.Kill();
+    }
+    
+    
+    
 }
