289da6b160ff77de3292b881e143c6199c14e563
raz hollander <raz.hollander@monkeyleague.io>
2022-07-16 20:51:31 +0300

    loot fix

--- Assets/Scripts/Money/Loot.cs
+++ Assets/Scripts/Money/Loot.cs
@@ -17,7 +17,9 @@ public class Loot : MonoBehaviour
     [SerializeField] private int _smallAmount = 100;
     [SerializeField] private int _mediumAmount = 500;
     [SerializeField] private int _bigAmount = 1000;
+    [SerializeField] private Collider2D _collider;
 
+    private bool isGiveAlready = false;
     private void Awake()
     {
         if (_moneyAmount <= _smallAmount)
@@ -40,10 +42,12 @@ public class Loot : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        Debug.Log(other.name);
+        if (other.CompareTag("Player") && !isGiveAlready)
         {
+            isGiveAlready = true;
             GameManager.Instance.MoneyManager.AddMoney(_moneyAmount);
             DoEffect();
         }
@@ -53,6 +57,7 @@ public class Loot : MonoBehaviour
     private void DoEffect()
     {
         _image.enabled = false;
+        _collider.enabled = false;
         _collectEffect.Play(_moneyAmount);
     }
     
