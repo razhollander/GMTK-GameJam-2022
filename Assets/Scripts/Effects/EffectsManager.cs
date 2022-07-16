cec32230700b8541285b2b7e72913216d0f657d8
raz hollander <raz.hollander@monkeyleague.io>
2022-07-16 12:12:52 +0300

    alarm

--- Assets/Scripts/Effects/EffectsManager.cs
+++ Assets/Scripts/Effects/EffectsManager.cs
@@ -28,6 +28,7 @@ public class EffectsManager : GaneEventListener
                 Vignette vignette;
  
                 if(!VolumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
+                vignette.active = true;
                 vignette.intensity.Override(_alarmAnimationBounds.x);
                 alarmAnimationTween = DOTween.To(() => vignette.intensity.value, value => vignette.intensity.Override(value), _alarmAnimationBounds.y, _alarmAnimationTime)
                     .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InQuart); break;
@@ -40,11 +41,20 @@ public class EffectsManager : GaneEventListener
     {
         OnGameEvent(GameEvent.Alarm);
     }
+    
+    [ContextMenu("fire freeze effect")]
+    public void FireFreeze()
+    {
+        OnGameEvent(GameEvent.Freeze);
+    }
     private void StopAll()
     {
         _snowEffect.gameObject.SetActive(false);
         _snowEffect.Stop();
         alarmAnimationTween.Kill();
+        Vignette vignette;
+        if(!VolumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
+        vignette.active = false;
     }
     
     
