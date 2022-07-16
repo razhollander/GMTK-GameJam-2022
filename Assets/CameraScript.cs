f3ab8b4408837ec211906952f31e74d92d26448b
MarMeron <meron.hadarr@gmail.com>
2022-07-16 10:28:37 +0300

    camera

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -1,20 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class CameraScript : MonoBehaviour
 {
     public Transform camera_head;
 
-    [SerializeField] GameObject light_spot;
-
-    private void Start()
-    {
-        //light_spot = GetComponent<>
-    }
+    public GameObject light_spot;
 
     private void Update()
     {
-        
+        Vector3 diff = camera_head.position - light_spot.transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        camera_head.rotation = Quaternion.Euler(0f, 0f, rot_z);
     }
 }
